a5405125f7ddc0b001b8180f2e4547f153f2c4a1
taharfi <rouifitaha@gmail.com>
2026-01-13 19:45:26 +0100

    fix: replace @ alias with relative import for Position in WalletImport

--- src/components/WalletImport.tsx
+++ src/components/WalletImport.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Position } from "@/types/position";
+import { Position } from "../types/position";
 
 interface WalletImportProps {
   onPositionLoaded: (position: Position) => void;
